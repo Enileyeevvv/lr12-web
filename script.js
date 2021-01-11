f5050578b9a9d2fed358100ce9c64355ba118a76
Enileyeevvv <i_enileyev@mail.ru>
2021-01-11 21:52:30 +0300

    еду

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-const app = {
+const App = {
   data() {
     return{
-
+      name:"",
+      surname:"",
+      isButtonDisabled:true,
+      list:[],
     };
   },
   methods:{
-    
+    nameDelt() {
+      this.name = event.target.value;
+      console.log(this.name);
+    },
+    surnameDelt() {
+      this.surname = event.target.value;
+      console.log(this.surname);
+    },
+    addNewNote() {
+        this.list.push(`
+        ${this.surname[0].toUpperCase() + this.surname.toLowerCase().slice(1)} ${this.name[0].toUpperCase() + this.name.toLowerCase().slice(1)}
+        `);
+        this.name="";
+        this.surname="";
+        console.log(this.list);
+    }
   },
   watch: {
+    name(value) {
+      if (value.length > 0 && this.surname.length > 0) {
+        this.isButtonDisabled = false;
+      } else {
+        this.isButtonDisabled = true;
+      }
+    },
 
+    surname(value) {
+      if (value.length > 0 && this.name.length > 0) {
+        this.isButtonDisabled = false;
+      } else {
+        this.isButtonDisabled = true;
+      }
+    },
   }
 };
 
